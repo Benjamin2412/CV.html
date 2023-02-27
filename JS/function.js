5d3195d1a072fae7951b9797525ae65d0efb0e58
BenjaminCardozo <124116263+Benjamin2412@users.noreply.github.com>
2023-02-27 16:55:29 -0300

    Delete function.js

--- JS/function.js
+++
@@ -1,5 +0,0 @@
-
-document.getElementById("boton").onclick = function () {
- console.log("Capturamos el evento click");
- document.getElementById("demo").innerHTML = "Estamos probando nuestro primer evento en JS";
-}
